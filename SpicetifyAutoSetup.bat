4260d82b2783f0414fbd30cb01d4cedcbed96ea7
NΞVΛR <63956787+NEVARLeVrai@users.noreply.github.com>
2022-12-29 14:01:39 +0100

    Update SpicetifyAutoSetup.bat

--- SpicetifyAutoSetup.bat
+++ SpicetifyAutoSetup.bat
@@ -13,19 +13,16 @@ echo ---------------------------------------------------------------------------
 echo Verify you executed:
 echo "set-executionpolicy RemoteSigned" before in a elevated powershell the first time
 echo.
-echo Programs must be in C:\SpicetifyAutoSetup\Files for that work!
-echo Folder must be in "C:" !
 echo ------------------------------------------------- ---------------------------------------------
 echo Setup in progress please wait...
-echo V.1s
+echo V.1.0.1s
 echo Thanks to SPICEFITY
 echo ---------------------------------------------------------------------------------------------
 echo.
-powershell -File "C:\SpicetifyAutoSetup\Files\SpicetifyAutoSetup.ps1"
+powershell -File SpicetifyAutoSetup.ps1 /s /b
 echo ---------------------------------------------------------------------------------------------
 echo Setup Finish ! 
-echo (if setup finish too fast that because you dont executed in a elevated powershell 
-echo "set-executionpolicy RemoteSigned")
+echo if setup finish too fast that because you dont executed in a elevated powershell 
+echo "set-executionpolicy RemoteSigned"
 echo ---------------------------------------------------------------------------------------------
 timeout 10
-
