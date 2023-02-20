932db9847b6830dc1d9d231079863ca39be69209
NΞVΛR <63956787+NEVARLeVrai@users.noreply.github.com>
2023-02-20 16:17:29 +0100

    Delete SpicetifyAutoSetup.ps1

--- SpicetifyAutoSetup.ps1
+++
@@ -1,8 +0,0 @@
-$Texte1="Autorisation:"
-Write-host $Texte1
-Get-ExecutionPolicy
-$Texte2=" "
-Write-host $Texte2
-iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex
-iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | iex
-spicetify upgrade
