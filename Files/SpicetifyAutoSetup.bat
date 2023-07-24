19efacdd36e5e88cbf1d21a0587ca0d310c2c7e9
NΞVΛR <danielo-24@hotmail.com>
2023-07-24 15:26:52 +0200

    update 1.0.3s

---
+++ Files/SpicetifyAutoSetup.bat
@@ -0,0 +1,28 @@
+@echo off
+:start
+color 4
+title Spicetify Auto Setup
+echo ---------------------------------------------------------------------------------------------
+echo.
+echo    	   /!   / :----- \      /    /\     .----.
+echo  	  / !  /  :__	  \    /    /  \    :____:
+echo 	 /  ! /	  :        \  /    /----\   :   \
+echo		/   !/    :-----    \/    /      \  :    \
+echo.
+echo ---------------------------------------------------------------------------------------------
+echo Verify you executed:
+echo "set-executionpolicy RemoteSigned" before in a elevated powershell the first time
+echo.
+echo ------------------------------------------------- ---------------------------------------------
+echo Setup in progress please wait...
+echo V.1.0.3s
+echo Thanks to SPICEFITY
+echo ---------------------------------------------------------------------------------------------
+echo.
+powershell -File SpicetifyAutoSetup.ps1 /s /b
+echo ---------------------------------------------------------------------------------------------
+echo Setup Finish ! 
+echo if setup finish too fast that because you dont executed in a elevated powershell 
+echo "set-executionpolicy RemoteSigned"
+echo ---------------------------------------------------------------------------------------------
+timeout 10
